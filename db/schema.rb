e18605c65b4655fb1b2852d09df8af48ae9b31b7
19861115 <ipewlm@hotmail.co.jp>
2015-01-25 23:50:51 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125133410) do
+ActiveRecord::Schema.define(version: 20150125144950) do
+
+  create_table "bodies", force: true do |t|
+    t.string   "name"
+    t.integer  "maker_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "bodies", ["maker_id"], name: "index_bodies_on_maker_id"
 
   create_table "makers", force: true do |t|
     t.string   "name"
