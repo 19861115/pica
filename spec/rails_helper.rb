ea7c938eec8fad042a856eed6bb5adf1ddd15b8e
19861115 <ipewlm@hotmail.co.jp>
2015-01-25 15:09:55 +0900

    rspec and factorygirl config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,4 +47,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # FactiryGirl
+  config.before :all do
+    FactoryGirl.reload
+  end
 end
