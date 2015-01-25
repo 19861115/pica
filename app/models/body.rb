5db14ccf3991666c4e85fb5f3b5d3fa5c349a146
seomasanori <ipewlm@hotmail.co.jp>
2015-01-26 01:30:00 +0900

    create mount

--- app/models/body.rb
+++ app/models/body.rb
@@ -1,3 +1,6 @@
 class Body < ActiveRecord::Base
   belongs_to :maker
+  has_many :mounts
+  has_many :lens_models, through: :mounts
+  alias_attribute :lenses, :lens_models
 end
--- app/models/mount.rb
+++ app/models/mount.rb
@@ -1,4 +1,5 @@
 class Mount < ActiveRecord::Base
   belongs_to :body
   belongs_to :lens_model
+  alias_attribute :lens, :lens_model
 end
