3fb194421d747c7271a0653ef6b93a0787732b7c
19861115 <ipewlm@hotmail.co.jp>
2015-01-28 22:32:48 +0900

    rails g model picture path:string exposure_time:string f_number:string focal_length:string iso:string body:references lens_model:references

---
+++ spec/factories/pictures.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :picture do
+    path "MyString"
+exposure_time "MyString"
+f_number "MyString"
+focal_length "MyString"
+iso "MyString"
+body nil
+lens_model nil
+  end
+
+end
