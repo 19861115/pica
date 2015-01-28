a51aeb5eee5bdca823bfe9d86f75a916d93ff32a
seomasanori <ipewlm@hotmail.co.jp>
2015-01-28 23:16:10 +0900

    add validation to lens

--- spec/models/lens_model_spec.rb
+++ spec/models/lens_model_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe LensModel, :type => :model do
   specify { expect(subject).to respond_to(:name) }
   specify { expect(subject).to respond_to(:maker) }
   specify { expect(subject).to respond_to(:bodies) }
+
+  describe '#name' do
+    it 'can not be blank' do
+      @lens.name = ' '
+      expect(subject).not_to be_valid
+    end
+  end
 end
