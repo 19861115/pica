2d37c987a9b4ba59dcbf271970fc7f7b2b56cbaa
seomasanori <ipewlm@hotmail.co.jp>
2015-01-28 22:44:04 +0900

    create picture

--- spec/factories/pictures.rb
+++ spec/factories/pictures.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :picture do
-    path "MyString"
-exposure_time "MyString"
-f_number "MyString"
-focal_length "MyString"
-iso "MyString"
-body nil
-lens_model nil
+    sequence(:path) { |n| "path_#{n}" }
+    sequence(:exposure_time) { |n| "exposure_time_#{n}" } 
+    sequence(:f_number) { |n| "f_number_#{n}" }
+    sequence(:focal_length) { |n| "focal_length_#{n}" }
+    sequence(:iso) { |n| "iso_#{n}" }
+    body
+    lens_model
   end
 
 end
