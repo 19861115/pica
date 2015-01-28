bd7a56c5b085c6d2e625d18c7167462bc64c9dc9
19861115 <ipewlm@hotmail.co.jp>
2015-01-28 22:44:04 +0900

    create picture

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Picture, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @picture = FactoryGirl.create(:picture) }
+
+  subject { @picture }
+
+  specify { expect(subject).to respond_to(:path) }
+  specify { expect(subject).to respond_to(:exposure_time) }
+  specify { expect(subject).to respond_to(:f_number) }
+  specify { expect(subject).to respond_to(:focal_length) }
+  specify { expect(subject).to respond_to(:iso) }
+  specify { expect(subject).to respond_to(:body) }
+  specify { expect(subject).to respond_to(:lens) }
 end
