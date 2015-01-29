892155bd889d01edc803e28d017647f7539e13ab
seomasanori <ipewlm@hotmail.co.jp>
2015-01-29 23:48:28 +0900

    create 'pictures/init' action

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -23,4 +23,10 @@ RSpec.describe PicturesController, :type => :controller do
     end
   end
 
+  describe "GET init" do
+    it "returns http success" do
+      get :init
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
