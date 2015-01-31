50e438f5d7cd6dc420afb303b4e2e48db7c49705
seomasanori <ipewlm@hotmail.co.jp>
2015-01-31 13:38:44 +0900

    create maker/body/lens when creating picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -14,9 +14,27 @@ class Picture < ActiveRecord::Base
           exposure_time: jpeg.exposure_time.to_s,
           f_number: jpeg.f_number.to_f.to_s,
           focal_length: jpeg.focal_length.to_s,
-          iso: jpeg.iso_speed_ratings.to_s
+          iso: jpeg.iso_speed_ratings.to_s,
+          body_id: get_body_id(jpeg.model, jpeg.make),
+          lens_model_id: get_lens_id(jpeg.lens_model, jpeg.lens_make)
         }
       end
     end
+  end
+
+  private
+
+  def get_body_id(model, make)
+    if make
+      maker = Maker.find_or_create_by(name: make.to_s)
+      Body.find_or_create_by(name: model.to_s, maker_id: maker.id).id if model
+    end 
+  end
+
+  def get_lens_id(lens_model, lens_make)
+    if lens_make
+      maker = Maker.find_or_create_by(name: lens_make.to_s)
+      LensModel.find_or_create_by(name: lens_model.to_s, maker_id: maker.id).id if lens_model
+    end 
   end
 end
