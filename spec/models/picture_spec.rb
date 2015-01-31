cad4ae6b2dcf0566b999c4d28c5f27abc300d03e
seomasanori <ipewlm@hotmail.co.jp>
2015-01-31 13:48:56 +0900

    fix exif default value nil->'undefined'

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -35,15 +35,75 @@ RSpec.describe Picture, :type => :model do
     end
 
     context "'no-exif.jpg'" do
-      specify 'set only path' do
+      specify 'set default exif-data' do
         picture = Picture.new(path: Rails.root.join('spec/dummy_pictures/no-exif.jpg').to_s)
         expect(picture.path).not_to be_nil
-        expect(picture.exposure_time).to be_nil
-        expect(picture.f_number).to be_nil
-        expect(picture.focal_length).to be_nil
-        expect(picture.iso).to be_nil
-        expect(picture.body).to be_nil
-        expect(picture.lens).to be_nil
+        expect(picture.exposure_time).to eq('undefined')
+        expect(picture.f_number).to eq('undefined')
+        expect(picture.focal_length).to eq('undefined')
+        expect(picture.iso).to eq('undefined')
+        expect(picture.body.name).to eq('undefined')
+        expect(picture.lens.name).to eq('undefined')
+      end
+    end
+
+    context "'exif-no-exposuretime.jpg'" do
+      specify 'set default exposuretime' do
+        picture = Picture.new(path: Rails.root.join('spec/dummy_pictures/exif-no-exposuretime.jpg').to_s)
+        expect(picture.exposure_time).to eq('undefined')
+      end
+    end
+
+    context "'exif-no-fnumber.jpg'" do
+      specify 'set default fnumber' do
+        picture = Picture.new(path: Rails.root.join('spec/dummy_pictures/exif-no-fnumber.jpg').to_s)
+        expect(picture.f_number).to eq('undefined')
+      end
+    end
+
+    context "'exif-no-focallength.jpg'" do
+      specify 'set default focallength' do
+        picture = Picture.new(path: Rails.root.join('spec/dummy_pictures/exif-no-focallength.jpg').to_s)
+        expect(picture.focal_length).to eq('undefined')
+      end
+    end
+
+    context "'exif-no-iso.jpg'" do
+      specify 'set default iso' do
+        picture = Picture.new(path: Rails.root.join('spec/dummy_pictures/exif-no-iso.jpg').to_s)
+        expect(picture.iso).to eq('undefined')
+      end
+    end
+
+    context "'exif-no-make.jpg'" do
+      specify 'set default body' do
+        picture = Picture.new(path: Rails.root.join('spec/dummy_pictures/exif-no-make.jpg').to_s)
+        expect(picture.body.name).not_to eq('undefined')
+        expect(picture.body.maker.name).to eq('undefined')
+      end
+    end
+
+    context "'exif-no-model.jpg'" do
+      specify 'set default body' do
+        picture = Picture.new(path: Rails.root.join('spec/dummy_pictures/exif-no-model.jpg').to_s)
+        expect(picture.body.name).to eq('undefined')
+        expect(picture.body.maker.name).not_to eq('undefined')
+      end
+    end
+
+    context "'exif-no-lensmake.jpg'" do
+      specify 'set default lens' do
+        picture = Picture.new(path: Rails.root.join('spec/dummy_pictures/exif-no-lensmake.jpg').to_s)
+        expect(picture.lens.name).not_to eq('undefined')
+        expect(picture.lens.maker.name).to eq('undefined')
+      end
+    end
+
+    context "'exif-no-lensmodel.jpg'" do
+      specify 'set default lens' do
+        picture = Picture.new(path: Rails.root.join('spec/dummy_pictures/exif-no-lensmodel.jpg').to_s)
+        expect(picture.lens.name).to eq('undefined')
+        expect(picture.lens.maker.name).not_to eq('undefined')
       end
     end
   end
