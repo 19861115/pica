4baadfedae1665eab7ba813054468e901c959dc9
seomasanori <ipewlm@hotmail.co.jp>
2015-02-05 14:31:07 +0900

    fix /pictures status spec

--- spec/requests/picture_pages_spec.rb
+++ spec/requests/picture_pages_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "PicturePages", :type => :request do
-  describe "GET /picture_pages" do
-    it "works! (now write some real specs)" do
-      get picture_pages_path
+  describe "GET /pictures" do
+    it "page exists" do
+      get pictures_index_path
       expect(response).to have_http_status(200)
     end
   end
