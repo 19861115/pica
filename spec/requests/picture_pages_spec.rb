2a6a59ba511a3bdfd9bc64b755e889ef9e3a8b5f
19861115 <ipewlm@hotmail.co.jp>
2015-02-05 15:35:37 +0900

    implement picture list area at /pictures

--- spec/requests/picture_pages_spec.rb
+++ spec/requests/picture_pages_spec.rb
@@ -6,5 +6,40 @@ RSpec.describe "PicturePages", :type => :request do
       get pictures_index_path
       expect(response).to have_http_status(200)
     end
+
+    specify "page title is 'Pictures'" do
+      visit pictures_index_path
+      expect(page).to have_title('Pictures')
+      expect(page).to have_content('Pictures')
+    end
+
+    context 'when some pictures registered' do
+      before do
+        10.times { FactoryGirl.create(:picture) }
+        visit pictures_index_path
+      end
+
+      specify "show registerd pictres" do
+        picture = Picture.first
+        expect(page).to have_content(picture.path)
+        expect(page).to have_content(picture.exposure_time)
+        expect(page).to have_content(picture.f_number)
+        expect(page).to have_content(picture.focal_length)
+        expect(page).to have_content(picture.iso)
+        expect(page).to have_content(picture.body.name)
+        expect(page).to have_content(picture.lens.name)
+      end
+    end
+
+    context 'when no picture registered' do
+      before do
+        Picture.delete_all
+        visit pictures_index_path
+      end
+
+      specify "show registerd pictres" do
+        expect(page).to have_content('no picture')
+      end
+    end
   end
 end
