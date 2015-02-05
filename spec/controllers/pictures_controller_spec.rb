136cb6203ab1f764bdb4587343b3211571e34144
seomasanori <ipewlm@hotmail.co.jp>
2015-02-05 15:54:26 +0900

    fix config/routes.db

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe PicturesController, :type => :controller do
 
   describe "GET show" do
     it "returns http success" do
-      get :show
+      get :show, params = { id: FactoryGirl.create(:picture).id }
       expect(response).to have_http_status(:success)
     end
   end
