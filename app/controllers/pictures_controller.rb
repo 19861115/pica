7f45f731c588c19151a9aa82bf0b596ebce40ff4
19861115 <ipewlm@hotmail.co.jp>
2015-02-07 19:14:16 +0900

    refactor charts-page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -31,58 +31,71 @@ class PicturesController < ApplicationController
   def charts
     if Picture.count > 0
       @pictures = Picture.includes(:body, :lens_model).all
-      # generate exposure_times hash
-      exposure_times = {}
-      @pictures.pluck(:exposure_time).uniq.each do |e|
-        exposure_times[e] = Picture.where(exposure_time: e).count
-      end
-      @exposure_times_sorted = exposure_times.sort_by { |key, val| to_f(key) }
-
-      # generate f_numbers hash
-      f_numbers = {}
-      @pictures.pluck(:f_number).uniq.each do |f|
-        f_numbers[f] = Picture.where(f_number: f).count
-      end
-      @f_numbers_sorted = f_numbers.sort_by { |key, val| key }
-
-      # generate focal_lengths hash
-      focal_lengthes = {}
-      @pictures.pluck(:focal_length).uniq.each do |f|
-        focal_lengthes[to_f(f)] = Picture.where(focal_length: f).count
-      end
-      @focal_lengthes_sorted = focal_lengthes.sort_by { |key, val| key }
-
-      # generate isoes hash
-      isoes = {}
-      @pictures.pluck(:iso).uniq.each do |i|
-        isoes[i] = Picture.where(iso: i).count
-      end
-      @isoes_sorted = isoes.sort_by { |key, val| key }
-
-      # generate bodies hash
-      bodies = {}
-      @pictures.pluck(:body_id).uniq.each do |b|
-        bodies[Body.find_by(id: b).name] = Picture.where(body_id: b).count
-      end
-      @bodies_sorted = bodies.sort_by { |key, val| key }
-
-      # generate lenses hash
-      lenses = {}
-      @pictures.pluck(:lens_model_id).uniq.each do |l|
-        lenses[LensModel.find_by(id: l).name] = Picture.where(lens_model_id: l).count
-      end
-      @lenses_sorted = lenses.sort_by { |key, val| key }
+      @exposure_times = exposure_times(@pictures)
+      @f_numbers = f_numbers(@pictures)
+      @focal_lengthes = focal_lengthes(@pictures)
+      @isoes = isoes(@pictures)
+      @bodies = bodies(@pictures)
+      @lenses = lenses(@pictures)
     end
   end
 
   private
 
   def to_f(val)
-    unless val == "undefined"
+    if val.include?('/')
       splitted = val.split('/')
       Rational(splitted[0], splitted[1]).to_f
     else
       0.0
     end
+  end
+
+  def exposure_times(pictures)
+    exposure_times = {}
+    pictures.pluck(:exposure_time).uniq.each do |e|
+      exposure_times[e] = Picture.where(exposure_time: e).count
+    end
+    exposure_times.sort_by { |key, val| to_f(key) }
+  end
+
+  def f_numbers(pictures)
+    f_numbers = {}
+    pictures.pluck(:f_number).uniq.each do |f|
+      f_numbers[f] = Picture.where(f_number: f).count
+    end
+    f_numbers.sort_by { |key, val| key }
+  end
+
+  def focal_lengthes(pictures)
+    focal_lengthes = {}
+    pictures.pluck(:focal_length).uniq.each do |f|
+      focal_lengthes[to_f(f)] = Picture.where(focal_length: f).count
+    end
+    focal_lengthes.sort_by { |key, val| key }
+  end
+
+  def isoes(pictures)
+    isoes = {}
+    pictures.pluck(:iso).uniq.each do |i|
+      isoes[i] = Picture.where(iso: i).count
+    end
+    isoes.sort_by { |key, val| key }
+  end
+
+  def bodies(pictures)
+    bodies = {}
+    pictures.pluck(:body_id).uniq.each do |b|
+      bodies[Body.find_by(id: b).name] = Picture.where(body_id: b).count
+    end
+    bodies.sort_by { |key, val| key }
+  end
+
+  def lenses(pictures)
+    lenses = {}
+    pictures.pluck(:lens_model_id).uniq.each do |l|
+      lenses[LensModel.find_by(id: l).name] = Picture.where(lens_model_id: l).count
+    end
+    lenses.sort_by { |key, val| key }
   end
 end
