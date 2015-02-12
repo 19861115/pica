1dfd233e6cb27b6dd9a1413bcd2e55ee6680e005
19861115 <ipewlm@hotmail.co.jp>
2015-02-13 00:40:45 +0900

    refactor others

--- spec/factories/pictures.rb
+++ spec/factories/pictures.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :picture do
     sequence(:path) { |n| "path_#{n}" }
-    sequence(:exposure_time) { |n| "exposure_time_#{n}" } 
+    sequence(:exposure_time) { |n| "exposure_time_#{n}" }
     sequence(:f_number) { |n| "f_number_#{n}" }
     sequence(:focal_length) { |n| "focal_length_#{n}" }
     sequence(:iso) { |n| "iso_#{n}" }
